88dac9d2ae8f55b16090e56cfe5d0481d6c1d17b
Aziza Brahimi <brahimiaziza377@gmail.fr>
2020-10-08 15:40:19 +0200

    rajouter le countdown de Warda

--- countdown.js
+++ countdown.js
@@ -1,13 +1,13 @@
 jQuery(function($){ 
-var lauch = new Date(2021,2,22,17,30,0);
+var launch = new Date(2021,2,22,17,30,0);
 var Days = $('#Days');
 var Hours = $('#Hours');
 var Minutes = $('#Minutes');
 var Secondes = $('#Secondes');
-setData();
-function setData(){
+setDate();
+function setDate(){
   var now = new Date();
-  var s = (lauch.getTime() - now.getTime())/1000;
+  var s = (launch.getTime() - now.getTime())/1000;
   var d = Math.floor(s/86400);
   Days.html('<strong>'+d+'</strong');
   s -= d*86400;
